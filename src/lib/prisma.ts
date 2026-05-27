ae70af7642254c212bbbf203ff2dee2050850fb3
Forge Security <forge-security@yoursiteguru.com>
2026-05-27 10:48:53 -0700

    security: migrate to Prisma 7 + realign Dockerfile for CVE-2025-55182

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 declare global {
   // eslint-disable-next-line no-var
   var __narratrackPrisma__: PrismaClient | undefined;
@@ -7,7 +8,7 @@ declare global {
 
 export function getPrisma() {
   if (!globalThis.__narratrackPrisma__) {
-    globalThis.__narratrackPrisma__ = new PrismaClient();
+    globalThis.__narratrackPrisma__ = new PrismaClient({ adapter: new PrismaBetterSqlite3({ url: process.env.DATABASE_URL ?? "file:./dev.db" }) });
   }
 
   return globalThis.__narratrackPrisma__;
